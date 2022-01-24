d5d21a30fd719b10521237d291d94adea83f2d5c
Chris Roselle <chrisroselle@gmail.com>
2022-01-24 15:49:59 -0500

    testing

--- library/cr-common/vars/p.groovy
+++ library/cr-common/vars/p.groovy
@@ -1,7 +1,8 @@
-import com.github.chrisroselle.ChoiceParam
+//import com.github.chrisroselle.ChoiceParam
 import groovy.transform.Field
 
-@Field example = new ChoiceParam()
-example.name = 'example'
-example.description = 'this is an example of a reusable parameter'
-example.choices = ['one', 'two']
+@Field example = [
+    name: 'example',
+    description: 'this is an example of a reusable parameter',
+    choices: ['one', 'two']
+]
