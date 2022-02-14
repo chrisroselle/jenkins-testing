2850c344a921c5267744f7bcfa4f0690c844d753
Chris Roselle <chrisroselle@gmail.com>
2022-02-14 09:26:22 -0500

    add method for getting log from build

--- library/cr-common/vars/cr_common.groovy
+++ library/cr-common/vars/cr_common.groovy
@@ -1,3 +1,9 @@
 import groovy.transform.Field
+import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
 
 @Field example = ['one', 'two']
+
+@NonCPS
+String getLogFromRunWrapper(RunWrapper runWrapper, int logLines) {
+    return runWrapper.getRawBuild().getLog(logLines)
+}
